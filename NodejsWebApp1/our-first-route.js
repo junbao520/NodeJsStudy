3e6f6158d15fa5f8ae967378ef03eaef3a2fdfeb
junbao <757114760@qq.com>
2020-07-13 17:33:49 +0800

    修改

--- NodejsWebApp1/our-first-route.js
+++ NodejsWebApp1/our-first-route.js
@@ -16,6 +16,27 @@ async function routes(fastify, options) {
         reply.send({ hello: 'test' })
     })
 
+    fastify.route({
+        method: 'GET',
+        url: '/testQuery',
+        schema: {
+          querystring: {
+            name: { type: 'string' },
+            excitement: { type: 'integer' }
+          },
+          response: {
+            200: {
+              type: 'object',
+              properties: {
+                hello: { type: 'string' }
+              }
+            }
+          }
+        },
+        handler: function (request, reply) {
+          reply.send({ hello: 'world' })
+        }
+      })
 
     fastify.route({
         method: 'GET',
@@ -26,7 +47,24 @@ async function routes(fastify, options) {
             reply.send({ text: 'hello fastify' })
         }
     })
+    //暂时无效
+// fastify.addHook('onRequest', (req, res, next) => {
+//   // some code
+//   console.log("onRequest");
+//   next()
+// })
 
+// fastify.addHook('preHandler', (request, reply, next) => {
+//   // some code
+//   console.log("preHandler");
+//   next()
+// })
+
+// fastify.addHook('onResponse', (res, next) => {
+//   // some code
+//   console.log('onReponse');
+//   next()
+// })
 
 
 
