a0d4f393ea96f23982bb2bb3593743873f8d173b
junbao <757114760@qq.com>
2020-07-16 11:43:17 +0800

    封装通用数据库CURD 接口！

--- NodejsWebApp1/fastify.js
+++ NodejsWebApp1/fastify.js
@@ -15,7 +15,7 @@ fastify.post('/login',  async function (request, reply) {
     }
     let altSignOptions = Object.assign({}, fastify.jwt.options.sign)
     altSignOptions.issuer = 'another.example.tld'
-    //有效期500秒
+    //有效期5000秒
     altSignOptions.expiresIn="5000000"
     const tokenAlt = await reply.jwtSign(data, altSignOptions)
 
@@ -27,21 +27,18 @@ fastify.post('/login',  async function (request, reply) {
     reply.send(replyData)
   }
 })
-//第四步 测试访问，需要 header authorization Bearer +Token 访问
-fastify.get('/test', function (request, reply) {
-  //获取token信息
-  console.log(request.data);
-  reply.send(request.data)
-})
-
 //允许通过的白名单 正则表达式匹配
 const whitelistedRoutes = [
   '/login',
   '/docs',
-  '/user/GetPageData'
+  '/user/GetPageData',
+  '/user/Add',
+  '/user/Update',
+  '/user/Del'
 ];
 
-//添加钩子函数 请求之前拦截,钩子函数的请求声明周期在中间件之前
+
+//第四步 添加钩子函数 请求之前拦截,钩子函数的请求声明周期在中间件之前
 //请求之前添加钩子函数进行Jwt 验证
 fastify.addHook('onRequest', async (request, reply) => {
   try {
@@ -50,11 +47,22 @@ fastify.addHook('onRequest', async (request, reply) => {
     })) {
       //await request.jwtVerify();
       request.jwtVerify(function (err, decoded) {
-        debugger;
         if(!err){
            //解密后的信息保存在Request中
            request.data=decoded;
            console.log(decoded);
+
+           //进行Request参数处理
+          //  {
+          //   "page": 1,
+          //   "rows": 30,
+          //   "sort": "CreateDate",
+          //   "order": "desc",
+          //   "wheres": "[]"
+          // }
+        }
+        else{
+          reply.send(err);
         }
       
       })
@@ -63,6 +71,14 @@ fastify.addHook('onRequest', async (request, reply) => {
     reply.send(err);
   }
 });
+
+//第五步 测试访问，需要 header authorization Bearer +Token 访问
+fastify.get('/test', function (request, reply) {
+  //获取token信息
+  console.log(request.data);
+  reply.send(request.data)
+})
+
 
 //注册路由
 fastify.register(require('./our-first-route'))
--- NodejsWebApp1/our-first-route.js
+++ NodejsWebApp1/our-first-route.js
@@ -9,12 +9,14 @@ var knex = require('knex')({
   version: '5.7',
   connection: {
     host: 'localhost',
-    port: '3307',
+    port: '3306',
     user: 'root',
     password: '123456',
     database: 'nodejsstudy'
   }
 });
+
+
 
 
 async function routes(fastify, options) {
@@ -30,34 +32,40 @@ async function routes(fastify, options) {
     let id = request.id;
     return id;
   })
+  //分页查询接口
   fastify.post('/user/GetPageData', async (request, reply) => {
-
     try {
-      debugger;
-      let toStringQuery = knex.select().from('sys_user').toString();
-
-      console.log("打印开始");
-     
-
-      knex.select().from('sys_user').asCallback(function (err, result) {
-        debugger;
-      
-       debugger;
-   
-      });
-
-      let res=await knex.select().from('sys_user');
-      console.log(res);
-
-      console.log("打印完成1");
+      let page = request.body.page;
+      let rows = request.body.rows;
+      let sort = request.body.sort;
+      let order=request.body.order;
+      let wheres = request.body.wheres;
+      let tableName ='sys_user';
+      let wheresIn=request.body.wheresIn;
+      console.log("打印开始2");
+      var query = knex.select().from(tableName);
+      //集成条件判断查询
+      if (wheres != '' && wheres != null) {
+        query = query.andWhere(wheres);
+      }
+      //集成In查询
+      if(wheresIn!=''&&wheresIn!=null){
+        query=query.whereIn(wheresIn.key, wheresIn.value)
+      }
+      //集成分页
+      query=query.limit(rows).offset((page-1)*rows).orderBy(sort, order);
+      //集成范围查询
+      let querySql =query.toString();
+      console.log(querySql);
+      let res = await query;
+      console.log("打印完成");
       console.log(new Date());
 
-
-      debugger;
-      console.log(toStringQuery);
-
-      debugger;
-      return res;
+      //返回结果
+      let WebResponseContent={status:false,code:'',message:'',data:''};
+      WebResponseContent.status=true;
+      WebResponseContent.data=res;
+      return WebResponseContent;
 
     } catch (error) {
       console.log(error);
@@ -65,7 +73,61 @@ async function routes(fastify, options) {
     }
 
   })
+  //添加数据接口
+  fastify.post('/user/Add', async (request, reply) => {
+    
+    //普通接口
+   // let tableName = request.body.tableName;
+    let tableName="sys_user";
+    let saveModel=request.body.saveModel;
+    saveModel.user_Id=123;
+    debugger;
+    //添加
+    let result=knex(tableName).insert(saveModel);
+    console.log(result.toString());
+    result=await result;
 
+
+    let WebResponseContent={status:false,code:'',message:'',data:''};
+    WebResponseContent.status=true;
+    WebResponseContent.data=result;
+    return WebResponseContent;
+
+
+  })
+  //更新数据接口
+  fastify.post('/user/Update', async (request, reply) => {
+    
+    //普通接口
+    let tableName="sys_user";
+    let saveModel=request.body.saveModel;
+    let wheres=request.body.wheres;
+    //saveModel.user_Id=123;
+    let result=knex(tableName).update(saveModel).where(wheres);
+    console.log(result.toString());
+    result=await result;
+
+    let WebResponseContent={status:false,code:'',message:'',data:''};
+    WebResponseContent.status=true;
+    WebResponseContent.data=result;
+    return WebResponseContent;
+
+  })
+ //删除数据接口
+  fastify.post('/user/Del', async (request, reply) => {
+    //普通接口
+    let tableName="sys_user";
+    let wheres=request.body.wheres;
+    //saveModel.user_Id=123;
+    let result=knex(tableName).where(wheres).del()
+    console.log(result.toString());
+    result=await result;
+    let WebResponseContent={status:false,code:'',message:'',data:''};
+    WebResponseContent.status=true;
+    WebResponseContent.data=result;
+    return WebResponseContent;
+  })
+ 
   fastify.route({
     method: 'GET',
     url: '/testQuery',
